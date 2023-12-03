169cca1c9b4e1c0ad81580e9eae1899117a830a3
Anton <2004lol222@mail.ru>
2023-12-03 23:22:07 +0800

    задача 1 с решением

---
+++ example_1/Program.cs
@@ -0,0 +1,20 @@
+﻿
+String aStr, bStr;
+Console.Write("Введите число 1: ");
+aStr = Console.ReadLine();
+Console.Write("Введите число 2: ");
+bStr = Console.ReadLine();
+
+int a = int.Parse(aStr);
+int b = int.Parse(bStr);
+
+if (a == b * b) {
+    Console.WriteLine("True");
+}
+else { Console.WriteLine("False");}
+
+
+
+
+
+
