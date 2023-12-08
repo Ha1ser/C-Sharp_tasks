30e8fcfa69ad86dc3fd215e8122c4a8f3f0eb723
Anton <2004lol222@mail.ru>
2023-12-08 10:36:49 +0800

    первая задача 2 семенара

---
+++ example_4_les2/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Напишите программу, которая принимает на вход
+трёхзначное число и удаляет вторую цифру этого
+числа. */
+
+//мое решение:
+/*Console.WriteLine("Введите трехзначное число: ");
+int n = int.Parse(Console.ReadLine());
+
+int a_1 = n / 100; 
+int a_2 = n % 10;
+
+Console.Write(a_1);
+Console.WriteLine(a_2);*/
+
+//решение общ
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(n / 100 * 10 + n % 10);
+
+
