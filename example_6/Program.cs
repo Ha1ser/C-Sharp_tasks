21bf12d321ecdb6e46b828335641bc0b2af8b543
Anton <2004lol222@mail.ru>
2023-12-08 11:33:34 +0800

    3 задача из 2 сем...

---
+++ example_6/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите 1 число");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите 2 число");
+int n2 = int.Parse(Console.ReadLine()!);
+
+if (n % n2 == 0) {Console.WriteLine("Кратно");}
+else {Console.WriteLine(n % n2);}
