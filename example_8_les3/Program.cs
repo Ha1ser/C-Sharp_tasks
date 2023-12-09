3aef5686ee3ddf69d9cbf758bb0533e2be128021
Anton <2004lol222@mail.ru>
2023-12-09 21:25:01 +0800

    1 задание из лекции 3

---
+++ example_8_les3/Program.cs
@@ -0,0 +1,39 @@
+﻿int[] MassNums(int size, int from, int to) {
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++) {
+        arr[i] = new Random().Next(from, to + 1);
+    }
+    return arr;
+}
+
+void Print(int[] arr){
+    int size = arr.Length;
+    for (int i = 0; i < size; i++){
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+string IfNum(int[] arr, int num) {
+
+    for (int i = 0; i < arr.Length; i++){
+        if (arr[i] == num) { 
+            return "Да";
+        }
+    }    
+    return "Нет";
+
+}
+
+
+
+int num = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+
+
+int[] mass = MassNums(num, start, stop);
+Print(mass);
+int number = int.Parse(Console.ReadLine()!);
+Console.WriteLine(IfNum(mass, number));
