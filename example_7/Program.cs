7abae172ca3843867d2e397e243d709e92438b66
Anton <2004lol222@mail.ru>
2023-12-09 23:20:24 +0800

    commit all

---
+++ example_7/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Напишите программу, которая выводит третью с конца
+цифру заданного числа или сообщает, что третьей цифры
+нет. */
+
+void ThirdNum(int num){
+    if (num < 100) {
+        Console.WriteLine("третьей цифры нет"); 
+        return;
+    }
+    //while (num > 999)
+      //  num /= 10;
+    Console.WriteLine(num % 10);
+    //return;
+    }
+
+
+int a = int.Parse(Console.ReadLine()!);
+ThirdNum(a);
